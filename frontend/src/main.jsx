154de8b5be276975bb2c703e26690707f0192879
Chetan00000 <139692970+Chetan00000@users.noreply.github.com>
2025-08-15 13:43:01 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import {Provider} from "react-redux"
 import { store } from './redux/store.js'
 
-export const serverUrl="https://real-time-chat-application-backend-5jur.onrender.com"
+export const serverUrl="https://real-time-chat-application-backend-1dt2.onrender.com"
 createRoot(document.getElementById('root')).render(
 <BrowserRouter>
 <Provider store={store}>
